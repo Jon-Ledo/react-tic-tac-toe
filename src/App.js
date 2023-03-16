8b5c9761c2de9e24f64b3786ea9296b2aa537c2f
JonLedo <jonledo.code@gmail.com>
2023-03-15 23:45:08 -0400

    basic code for a working tictactoe game. Lots of room for improvement

--- src/App.js
+++ src/App.js
@@ -1,7 +1,72 @@
+import { useState, useEffect } from 'react'
+import Cell from './components/Cell'
+
+import './normalize.css'
 import './App.css'
 
 function App() {
-  return <div className='App'>Hello</div>
+  const [cells, setCells] = useState(['', '', '', '', '', '', '', '', ''])
+  const [go, setGo] = useState('circle')
+  const [winningMessage, setWinningMessage] = useState(null)
+
+  const message = `It is now ${go}'s play.`
+
+  useEffect(() => {
+    checkScore()
+  }, [cells])
+
+  const checkScore = () => {
+    const winningCombos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ]
+
+    winningCombos.forEach((array) => {
+      let circleWins = array.every((cell) => cells[cell] === 'circle')
+
+      if (circleWins) {
+        setWinningMessage('Circle Wins!')
+        return
+      }
+    })
+
+    winningCombos.forEach((array) => {
+      let crossWins = array.every((cell) => cells[cell] === 'cross')
+
+      if (crossWins) {
+        setWinningMessage('Cross Wins!')
+        return
+      }
+    })
+  }
+
+  return (
+    <div className='app'>
+      <div className='gameboard'>
+        {cells.map((cell, index) => {
+          return (
+            <Cell
+              key={index}
+              id={index}
+              cell={cell}
+              cells={cells}
+              setCells={setCells}
+              go={go}
+              setGo={setGo}
+              winningMessage={winningMessage}
+            />
+          )
+        })}
+      </div>
+      <p>{winningMessage || message}</p>
+    </div>
+  )
 }
 
 export default App
